6f6b726d13c4c249165001a857f3af276407aab2
Fair & Square Synthesis <thom@sqrsy.com>
2024-11-15 21:34:42 +1100

    Output C++ code to single file

--- run.R
+++ run.R
@@ -1,11 +1,40 @@
 library(tuneR)
-setwd("C:/Users/61436/Dropbox/Hobbies/Wavs/envelopes")
+setwd("C:/Users/61436/Dropbox/Hobbies/Electronics/GitHub/wav2h")
 source("wav2h.R")
 
-all_wavs <-
-  list.files("_wav out/", pattern = "\\.wav", full.names = TRUE)
+MOVING_AVERAGE_WINDOW = 100
+SAVE_ONE_IN_EVERY = 20
+PROJECT_FOLDER = "household-percussion/"
+WAV_FOLDER = "_wav out/"
+OUTPUT_DIR = "_h out/"
+OUTPUT_FILE = "sample_bank.h"
+CPP_OBJECT_TYPE = "byte"
+MAX_VALUE = 255
 
+###
+# Set up
+
+dir.create(PROJECT_FOLDER %+% OUTPUT_DIR)
+sink(PROJECT_FOLDER %+% OUTPUT_DIR %+% OUTPUT_FILE) # erase file
+cat("")
+sink()
+
+###
+# Load in all WAV files
+
+all_wavs <- list.files(PROJECT_FOLDER %+% WAV_FOLDER,
+                       pattern = "\\.wav",
+                       full.names = TRUE)
+
+###
+# Convert to C++ code
+
+list_of_all_object_names = vector("list", length(all_wavs))
+list_of_all_object_sizes = vector("list", length(all_wavs))
+i = 1
 lapply(all_wavs, function(wav_file) {
+  file_prefix = gsub(".wav", "", basename(wav_file))
+  
   # read in wav and rectify
   wav <- mono(readWave(wav_file))
   wav <- abs(wav)
@@ -14,17 +43,43 @@ lapply(all_wavs, function(wav_file) {
   plot(wav)
   
   # add some zeros so envelope doesn't rise too sharply...
-  wav@left <- c(rep(0, 100), wav@left)
-  to_output <- moving_average(wav, 100)
+  wav@left <- c(rep(0, MOVING_AVERAGE_WINDOW), wav@left)
+  to_output <- moving_average(wav, MOVING_AVERAGE_WINDOW)
   plot(to_output)
   
+  cpp_code <-
+    as_cpp(
+      to_output,
+      file_prefix,
+      reduction_factor = SAVE_ONE_IN_EVERY,
+      object_type = CPP_OBJECT_TYPE,
+      max_int_size = MAX_VALUE
+    )
+  
   # save C++ object
-  sink(paste0("_h out/", gsub(".wav", ".h", basename(wav_file))))
-  cat(as_cpp(
-    to_output,
-    gsub(".wav", "", basename(wav_file)),
-    reduction_factor = 100,
-    max_int_size = 3999
-  ))
+  sink(PROJECT_FOLDER %+% OUTPUT_DIR %+% OUTPUT_FILE, append = TRUE)
+  cat(cpp_code$cpp_code)
+  cat("\n")
   sink()
+  
+  # save object details for later
+  list_of_all_object_names[[i]] <<- cpp_code$object_name
+  list_of_all_object_sizes[[i]] <<- cpp_code$object_size
+  i <<- i + 1
 })
+
+###
+# Finish with pointer array
+
+sink(PROJECT_FOLDER %+% OUTPUT_DIR %+% OUTPUT_FILE, append = TRUE)
+cat(CPP_OBJECT_TYPE %+% "* sample_array[] = {")
+cat(paste0(list_of_all_object_names, collapse = ", "))
+cat("};")
+cat("\n")
+cat("\n")
+
+cat("int sample_sizes[] = {")
+cat(paste0(list_of_all_object_sizes, collapse = ", "))
+cat("};")
+cat("\n")
+sink()
--- wav2h.R
+++ wav2h.R
@@ -1,5 +1,8 @@
+`%+%` <- function(a, b)
+  paste0(a, b)
+
 setGeneric("moving_average",
-           function(x, n = 5){
+           function(x, n = 5) {
              res <- as.numeric(filter(x, rep(1 / n, n), sides = 2))
              res <- res[!is.na(res)]
              res
@@ -7,25 +10,25 @@ setGeneric("moving_average",
 
 # compute a moving average
 setMethod("moving_average", "Wave",
-          function(x, n = 5){
+          function(x, n = 5) {
             x@left = moving_average(x@left, n = n)
             x
           })
 
 # scale between -1 and 1
-scale.Wave <- function(x, center = NA, scale = NA){
+scale.Wave <- function(x, center = NA, scale = NA) {
   x@left = x@left / max(abs(x@left), na.rm = TRUE)
   x
 }
 
 # take absolute value
-abs.Wave <- function(x){
+abs.Wave <- function(x) {
   x@left = ifelse(x@left <= 0, 0, x@left)
   x
 }
 
 # remove leading and lagging silence
-trim <- function(x, min_volume = 0.01){
+trim <- function(x, min_volume = 0.01) {
   start = head(which(x@left > min_volume), 1)
   end = tail(which(x@left > min_volume), 1)
   x = x[start:end]
@@ -35,39 +38,44 @@ trim <- function(x, min_volume = 0.01){
 # split a wave into a list waves, similar to strsplit() method:
 #  > strips out any audio quieter than min_volume argument
 #  > removes any segments shorter than min_wave_size
-wavsplit <- function(x, min_volume = 0, min_wave_size = x@samp.rate/10){
-  
-  # assign every element in wave a group:
-  #  when zero is encountered, move on to next group
-  group_vector = vector("numeric", length(x@left))
-  group_label = 1
-  for(i in 2:length(x@left)){
-    if(abs(x@left[i]) <= min_volume){
-      group_vector[i] = 0
-      if(abs(x@left[i-1]) > min_volume){
-        group_label = group_label + 1
+wavsplit <-
+  function(x,
+           min_volume = 0,
+           min_wave_size = x@samp.rate / 10) {
+    # assign every element in wave a group:
+    #  when zero is encountered, move on to next group
+    group_vector = vector("numeric", length(x@left))
+    group_label = 1
+    for (i in 2:length(x@left)) {
+      if (abs(x@left[i]) <= min_volume) {
+        group_vector[i] = 0
+        if (abs(x@left[i - 1]) > min_volume) {
+          group_label = group_label + 1
+        }
+      } else{
+        group_vector[i] = group_label
       }
-    }else{
-      group_vector[i] = group_label
     }
+    
+    # tally how big each group is
+    #  (ignore group 0 since that is space between groups)
+    # then select the large groups
+    group_sizes = table(group_vector[group_vector != 0])
+    large_groups = group_sizes[group_sizes > min_wave_size]
+    large_groups = as.numeric(names(large_groups))
+    
+    # split up the wav and return a list of fragments
+    lapply(large_groups, function(g) {
+      x[which(group_vector == g)]
+    })
   }
-  
-  # tally how big each group is
-  #  (ignore group 0 since that is space between groups)
-  # then select the large groups
-  group_sizes = table(group_vector[group_vector!=0])
-  large_groups = group_sizes[group_sizes > min_wave_size]
-  large_groups = as.numeric(names(large_groups))
-  
-  # split up the wav and return a list of fragments
-  lapply(large_groups, function(g){
-    x[which(group_vector == g)]
-  })
-}
 
 # print values of wave as a C++ object definition
-as_cpp <- function(x, label, reduction_factor = 1, max_int_size = 999){
-  
+as_cpp <- function(x,
+                   label,
+                   reduction_factor = 1,
+                   object_type = "byte",
+                   max_int_size = 255) {
   # down-sample wave by taking every k-th value
   reduction_flag = 1:length(x) %% reduction_factor
   include = which(reduction_flag == 0)
@@ -78,9 +86,21 @@ as_cpp <- function(x, label, reduction_factor = 1, max_int_size = 999){
   int_size = length(values_to_write)
   
   # print!!
-  paste0("short WAV",
-         label,
-         "[", int_size, "] = {",
-         paste0(values_to_write, collapse = ", "),
-         "};\n")
+  list(
+    "object_name" = "sample_" %+% label,
+    
+    "object_size" = int_size,
+    
+    "cpp_code" = paste0(
+      "const ",
+      object_type,
+      " sample_",
+      label,
+      "[",
+      int_size,
+      "] = {",
+      paste0(values_to_write, collapse = ", "),
+      "};\n"
+    )
+  )
 }
